067f3782efbd295465135149c55ed396cfee9c69
hyber1z0r <jakobsky@live.dk>
2016-12-09 12:01:32 +0100

    Add Contract solution back again

--- ContractsAss1/Account.cs
+++ ContractsAss1/Account.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Conditions;
+using System.Diagnostics.Contracts;
 
 namespace ContractsAss1
 {
@@ -15,19 +16,39 @@ namespace ContractsAss1
 
         public void Withdraw(double amount)
         {
+            // Condition solution
             Condition.Requires(Balance - amount, "candidateBalance")
                 .IsGreaterOrEqual(0, "Amount to large");
 
             Condition.Requires(amount, "amount")
-                .IsGreaterThan(0, "You can't withdraw a negative amount - thats depositing you fool");
+                .IsLessOrEqual(0, "You can't withdraw a negative amount - thats depositing you fool");
 
+            // Contracts solution
+//          if (amount <= 0)
+//              throw new StupidException("You can't withdraw a negative amount - thats depositing you fool");
+//
+//          if (Balance - amount < 0)
+//              throw new InsufficientFundsException("Amount to large");
+//
+//          Contract.Requires<StupidException>(amount > 0);
+//          Contract.Requires<InsufficientFundsException>(Balance - amount >= 0);
+//          Contract.EnsuresOnThrow<StupidException>(Contract.OldValue(Balance) == Balance);
+//          Contract.EnsuresOnThrow<InsufficientFundsException>(Contract.OldValue(Balance) == Balance);
             Balance -= amount;
         }
 
         public void Deposit(double amount)
         {
+            // Condition solution
             Condition.Requires(amount, "amount")
                 .IsGreaterThan(0, "You can't deposit a negative amount - thats withdrawing you fool");
+
+            // Contract solution
+//          if (amount <= 0)
+//              throw new StupidException("You can't withdraw a negative amount - thats depositing you fool");
+//          Contract.Requires<StupidException>(amount > 0);
+//          Contract.EnsuresOnThrow<StupidException>(Contract.OldValue(Balance) == Balance);
+
             Balance += amount;
         }
     }
