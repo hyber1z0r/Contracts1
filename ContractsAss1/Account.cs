d0b592e6399abf11a4591906c0e0e7b3f024dce4
hyber1z0r <jakobsky@live.dk>
2016-12-09 11:15:04 +0100

    Initial commit

---
+++ ContractsAss1/Account.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Conditions;
+
+namespace ContractsAss1
+{
+    public class Account
+    {
+        public double Balance { get; set; }
+
+        public Account()
+        {
+            Balance = 0.0;
+        }
+
+
+        public void Withdraw(double amount)
+        {
+            Condition.Requires(Balance - amount, "candidateBalance")
+                .IsGreaterOrEqual(0, "Amount to large");
+
+            Condition.Requires(amount, "amount")
+                .IsGreaterThan(0, "You can't withdraw a negative amount - thats depositing you fool");
+
+            Balance -= amount;
+        }
+
+        public void Deposit(double amount)
+        {
+            Condition.Requires(amount, "amount")
+                .IsGreaterThan(0, "You can't deposit a negative amount - thats withdrawing you fool");
+            Balance += amount;
+        }
+    }
+
+    public class StupidException : Exception
+    {
+        public StupidException(string message) : base(message)
+        {
+        }
+    }
+
+    public class InsufficientFundsException : Exception
+    {
+        public InsufficientFundsException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ ContractsAss1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ContractsAss1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var acc = new Account();
+            acc.Withdraw(-100);
+        }
+    }
+}
